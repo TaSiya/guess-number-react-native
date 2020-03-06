7124c4cf1966bbcf3d5e5656c2488fa1386a5f54
Siya Makhalima <siya@shark-2.local>
2020-03-06 21:11:11 +0200

    added input component and also manage to make it have dynamic style depending where its being used!

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,29 +1,41 @@
-import React from 'react'
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
-import Card from '../components/Card'
-import Colors from '../constants/color'
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import Card from '../components/Card';
+import Colors from '../constants/color';
+import Input from '../components/Input';
 
-const  StartGameScreen = props => {
+const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start A New Game </Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <TextInput  />
+        <Input
+          bluOnSubmit
+          autoCapitalize='none'
+          authoCOrrect={false}
+          keyboardType='number-pad'
+          maxLength={2}
+          style={styles.input}
+        />
         <View style={styles.buttonContainer}>
-          <View style={styles.buttonSize}><Button  title="reset" onPress={() => {}} color={Colors.accent}/></View>
-          <View style={styles.buttonSize}><Button  title="Confirm" onPress={() => {}} color={Colors.primary} /></View>
+          <View style={styles.buttonSize}>
+            <Button title='reset' onPress={() => {}} color={Colors.accent} />
+          </View>
+          <View style={styles.buttonSize}>
+            <Button title='Confirm' onPress={() => {}} color={Colors.primary} />
+          </View>
         </View>
       </Card>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: 'center',
+    alignItems: 'center'
   },
   title: {
     fontSize: 20,
@@ -36,14 +48,17 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flexDirection: 'row',
-    width: '100%', 
+    width: '100%',
     justifyContent: 'space-between',
     paddingHorizontal: 15
   },
   buttonSize: {
     width: '40%'
+  },
+  input: {
+    width: '50%',
+    textAlign: 'center'
   }
-})
+});
 
-export default StartGameScreen
-
+export default StartGameScreen;
