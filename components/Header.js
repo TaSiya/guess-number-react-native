19654ff38b1746ddd88ff72b7bb0623fa20041a1
Siya Makhalima <siya@shark-2.local>
2020-03-06 14:20:31 +0200

    added more to the start screen, able to add input and two buttons WIP

--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,13 @@ import { View, Text, StyleSheet} from 'react-native'
 
 const Header = props => {
   return (
-    <View style={style.header}>
-      <Text style={style.headerTitle}>{props.title}</Text>
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>{props.title}</Text>
     </View>
   )
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   header: {
     width: '100%',
     height: 90,
@@ -21,7 +21,7 @@ const style = StyleSheet.create({
   headerTitle: {
     color: 'black',
     fontSize: 18,
-    
+
   }
 })
 
